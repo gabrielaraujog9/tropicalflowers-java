308c1b9eb85b95392ffc65d02bbe97a142e650e1
gabrielaraujog9 <gabrielaraujogoncalves14@gmail.com>
2023-06-09 22:01:33 -0300

    criação tela de produto

--- backend/src/main/java/com/tropical/flowers/tropicalflowers/security/SecurityConfiguration.java
+++ backend/src/main/java/com/tropical/flowers/tropicalflowers/security/SecurityConfiguration.java
@@ -15,8 +15,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -49,6 +53,16 @@ public class SecurityConfiguration {
                .authenticationProvider(authProvider())
                .build();
   }
+  
+  //  @Bean
+  //   CorsConfigurationSource corsConfigurationSource() {
+  //       CorsConfiguration configuration = new CorsConfiguration();
+  //       configuration.setAllowedOrigins(Arrays.asList("*"));
+  //       configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
+  //       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+  //       source.registerCorsConfiguration("/**", configuration);
+  //       return source;
+  //  }
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
